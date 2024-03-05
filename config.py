8357f7d0cefd3982d3da77910b3ca58d9e677e46
Stefan Angerer <angerer.st@gmail.com>
2024-03-05 11:06:15 +0100

    test

--- config.py
+++ config.py
@@ -7,21 +7,21 @@ config = {
         "patch_folder": r'C:\Users\s.angerer\Privat\Studium\veg_classification\patches\bands',
         "extract_patches": "True",
         "patch_size": 1.00,
-        "cluster_size": "cl2",
+        "cluster_size": "cl_good",
         "t5_cov_herb_threshold": 20
     },
     "feature_extraction": {
-        "include_bands": True,
+        "include_bands": False,
         "specific_bands": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-        "include_indices": True,
+        "include_indices": False,
         "vi_patch_folder": r'C:\Users\s.angerer\Privat\Studium\veg_classification\patches\bands_vi',
         "indices": ["NDVI", "SAVI", "PSRI", "GNDVI", "NDRE", "VARI", "SR", "MCARI", "NDGI", "PRI"], 
-        "include_textures": True,
+        "include_textures": False,
         "textures": ['contrast', 'dissimilarity', 'homogeneity', 'energy', 'correlation'], #
         "statistics": ['mean', 'max', 'min', 'std', 'percentile_75', 'percentile_25', 'percentile_50'], #
     },
     "classification": {
-        "enable_rf": True,
+        "enable_rf": False,
         "results_folder": r"C:\Users\s.angerer\Privat\Studium\veg_classification\results\28_02_24\test_cl2",
         "include_hyperparameter_tuning": False,
         "hyperparameters": {
