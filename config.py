93128718b3c298d23db84702992c7460a09d2132
Stefan Angerer <angerer.st@gmail.com>
2024-03-14 10:49:01 +0100

    test

--- config.py
+++ config.py
@@ -11,11 +11,11 @@ def save_config_to_file(config, results_folder):
 
 config = {
     "data_paths": {
-        "ortho_tiff": r'C:\Users\s.angerer\Privat\Studium\veg_classification\input_data\Ortho_Schrankogel_32632_6cm.tif',
+        "ortho_tiff": r'D:\DATA\Schrankogel\Metashape_Project_Schrankogel\Ortho\bigtiff_LZW\Ortho_Schrankogel_32632_6cm.tif',
         "vegetation_gpkg": r'C:\Users\s.angerer\Privat\Studium\veg_classification\input_data\vegetation_groups_900plots_EPSG25832.gpkg'
     },
     "patch_creation": {
-        "patch_folder": r'C:\Users\s.angerer\Privat\Studium\veg_classification\patches\bands',
+        "patch_folder": r'D:\DATA\Masterthesis Angerer\veg_learn\veg-learn\patches',
         "extract_patches": True,
         "patch_size": 1.00,
         "cluster_size": "cl_good",
@@ -27,7 +27,7 @@ config = {
         "include_bands": True,
         "specific_bands": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
         "include_indices": True,
-        "vi_patch_folder": r'C:\Users\s.angerer\Privat\Studium\veg_classification\patches\bands_vi',
+        "vi_patch_folder": r'D:\DATA\Masterthesis Angerer\veg_learn\veg-learn\patches\bands_vi',
         "indices": ["NDVI", "SAVI", "PSRI", "GNDVI", "NDRE", "VARI", "SR", "MCARI", "NDGI", "PRI"], 
         "include_textures": True,
         "textures": ['contrast', 'dissimilarity', 'homogeneity', 'energy', 'correlation'], #
@@ -35,7 +35,7 @@ config = {
     },
     "classification": {
         "enable_rf": True,
-        "results_folder": r"C:\Users\s.angerer\Privat\Studium\veg_classification\results\13_03_24\test",
+        "results_folder": r"D:\DATA\Masterthesis Angerer\veg_learn\veg-learn\results\14_03_24\test",
         "include_hyperparameter_tuning": False,
         "cv_type": 'stratified', #stratified or leave_one_out
         "hyperparameters": {
